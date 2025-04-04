6207fa8698d4bcc793179ab048b1d399b22e1eb6
JD <barrerasjuan01@gmail.com>
2025-04-03 17:04:00 -0700

    added  HTTP client

--- src/github/Main.java
+++ src/github/Main.java
@@ -1,7 +1,25 @@
 package github;
 
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+
+
+        Scanner scanner = new Scanner(System.in);
+        String username = scanner.nextLine();
+        scanner.close();
+        String url = "https://api.github.com/users/"+username+"/repos";
+
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .build();
+
 
     }
 }
